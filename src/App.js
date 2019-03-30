8a830993e9378932acbae583b1d86eb5054c862e
dalton salmonjr <daltons09@gmail.com>
2019-03-29 23:41:35 -0400

    sign in enabled + authenticator for firebase

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import * as firebase from 'firebase';
 import RoomList from './components/RoomList';
 import MessageList from './components/MessageList';
-
+import User from './components/User';
 
 // Initialize Firebase
   var config = {
@@ -24,21 +24,29 @@ class App extends Component {
     };
   }
   
-  setActiveRoom(room) {
+setActiveRoom(room) {
     this.setState({activeRoom: room});
   }
   
-  render() {
+setUser(user) {
+    this.setState({user: user});
+    }  
+  
+render() {
     return (
       <div className="App">
         <aside id="sidebar">
           <h1 className="App-title">Bloc Chat</h1>
           <RoomList firebase={firebase} activeRoom={this.state.activeRoom} setActiveRoom={this.setActiveRoom.bind(this)} />
         </aside>
+        <aside id="sidebar-bottom">
+            <User firebase={firebase} setUser={this.setUser.bind(this)} user={this.state.user}/>
+        </aside>
         <MessageList firebase={firebase} activeRoom={this.state.activeRoom} />
       </div>
-    );
+      );
+    }
   }
-}
 
-export default App;
+  export default App;
+  
---
+++ src/components/User.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class User extends Component {
+
+componentDidMount() {
+	this.props.firebase.auth().onAuthStateChanged( user => {
+	this.props.setUser(user);
+	});
+}
+	
+signIn() {
+	const provider = new this.props.firebase.auth.GoogleAuthProvider();
+	this.props.firebase.auth().signInWithPopup( provider );
+}
+	
+signOut() {
+	this.props.firebase.auth().signOut();
+}
+	
+render() {
+	return (
+		<section className="user-display">
+			<div className="user-display-name">{ this.props.user ? this.props.user.displayName : '' }</div>
+			<button className="sign-in-out" onClick={ this.props.user ? this.signOut.bind(this) : this.signIn.bind(this) }>
+					<span>Sign { this.props.user ? 'out' : 'in' }</span>
+			</button>
+		</section>
+		);
+	}
+}
+
+export default User;
